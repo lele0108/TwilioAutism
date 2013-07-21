7d9a7bf58d5faa22a1be8e18e641d49327f4ede8
Jimmy Liu <sicong.liu98@gmail.com>
2013-07-21 11:14:52 -0700

    push

--- speech-recognizer.js
+++ speech-recognizer.js
@@ -48,29 +48,32 @@
                     console.log(event.results[i][0].transcript);
 
                     //CHECKS HERE
-                    if (event.results[i][0].transcript === "could you call my mom" || event.results[i][0].transcript  === "can you call my mom") {
-                        speak("Sure, I'm currently calling your mom for you");
+                    if (event.results[i][0].transcript === "could you call my mom") {
+                        //speak("Sure, I'm currently calling your mom for you");
                         $('#mom').addClass('animated fadeInDown');
                         $('#mom').toggle().fadeIn("slow");
+                        var query = "Sure Im currently calling your mom for you";
+                        var url = "http://tts-api.com/tts.mp3?q="+query;
+                        playSound(url);
                         Twilio.Device.connect();
                     }
 
-                    else if (event.results[i][0].transcript === "hello") {
-                        speak("Hello, how are you? I feel meowtastic.", { speed: 125 });
-                        $('#hi').addClass('animated fadeInDown');
-                        $('#hi').toggle();
-                    }
-
                     else if (event.results[i][0].transcript === "I want to buy some coffee") {
-                        speak("Here, try this: 1. Go up to cashier 2. Tell her what you want 3. Pay her what she tells you to 4. Get your delicious coffee! Do you want me to say it for you?", { speed: 125 });
+                        //speak("Here, try this: 1. Go up to cashier 2. Tell her what you want 3. Pay her what she tells you to 4. Get your delicious coffee! Do you want me to say it for you?", { speed: 125 });
                         $('#coffee').addClass('animated fadeInDown');
                         $('#coffee').toggle();
+                        var query = "Here, try this: 1. Go up to cashier 2. Tell her what you want 3. Pay her what she tells you to 4. Get your delicious coffee! Do you want me to say it for you?";
+                        var url = "http://tts-api.com/tts.mp3?q="+query;
+                        playSound(url);
                     }
 
                     else if (event.results[i][0].transcript === "I'm lost") {
-                        speak("Here are directions home from your location. Do you want me to ask your mom for help?");
+                        //speak("Here are directions home from your location. Do you want me to ask your mom for help?");
                         $('#lost').addClass('animated fadeInDown');
                         $('#lost').toggle();
+                        var query = "Here are directions home from your location. Do you want me to ask your mom for help?";
+                        var url = "http://tts-api.com/tts.mp3?q="+query;
+                        playSound(url);
                     }
 
                     else if (event.results[i][0].transcript === "i see a beautiful girl") {
@@ -80,21 +83,36 @@
                     }
 
                     else if (event.results[i][0].transcript === "I spilled something") {
-                        speak("Here, try cleaning it up with the towels. Ask some adults near you for help. Do you want to ask your mom for help?");
+                        //speak("Here, try cleaning it up with the towels. Ask some adults near you for help. Do you want to ask your mom for help?");
                         $('#spill').addClass('animated fadeInDown');
                         $('#spill').toggle();
+                        var query = "Here, try cleaning it up with the towels. Ask some adults near you for help. Do you want to ask your mom for help?";
+                        var url = "http://tts-api.com/tts.mp3?q="+query;
+                        playSound(url);
                     }
 
                     else if (event.results[i][0].transcript === "I'm bleeding") {
-                        speak("Here, don't worry. Try washing up with water in the restroom. After that you can put on a bandaid. Do you want to ask your mom for help?");
+                        //speak("Here, don't worry. Try washing up with water in the restroom. After that you can put on a bandaid. Do you want to ask your mom for help?");
                         $('#hurt').addClass('animated fadeInDown');
                         $('#hurt').toggle();
+                        var query = "Here, don't worry. Try washing up with water in the restroom. After that you can put on a bandaid. Do you want to ask your mom for help?";
+                        var url = "http://tts-api.com/tts.mp3?q="+query;
+                        playSound(url);
                     }
 
                     else if (event.results[i][0].transcript === "I'm bored") {
-                        speak("Here, lets draw a picture and send it to your mom");
+                        //speak("Here, lets draw a picture and send it to your mom");
                         $('#bored').addClass('animated fadeInDown');
                         $('#bored').toggle();
+                        var query = "Here, lets draw a picture and send it to your mom";
+                        var url = "http://tts-api.com/tts.mp3?q="+query;
+                        playSound(url);
+                    }
+
+                    else if (event.results[i][0].transcript === "hello") {
+                        speak("Hello, how are you? I feel meowtastic.", { speed: 125 });
+                        $('#hi').addClass('animated fadeInDown');
+                        $('#hi').toggle();
                     }
 
                     else if (event.results[i][0].transcript === "yes") {
@@ -105,13 +123,19 @@
                     }
 
                     else {
-                        speak("I did not quite get that, could you say it again?");
+                        var query = "I did not quite get that, can you say it again";
+                        var url = "http://tts-api.com/tts.mp3?q="+query;
+                        playSound(url);
                     }
 
                     recognition.stop();
                 } 
             }
         };
+
+        function playSound( url ){   
+          document.getElementById("audio_play").innerHTML="<embed src='"+url+"' hidden=true autostart=true loop=false>";
+        }
 
         recognition.onend = function() {
             $('.speech-content-mic').removeClass('speech-mic-works').addClass('speech-mic');
